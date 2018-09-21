da2810dd35f6210cffeee510fae11435542bb7d0
Amar Mujumdar <amujumdar@ucvts.tec.nj.us>
2018-09-21 14:47:30 -0400

    First two methods

--- src/ProblemSet2.java
+++ src/ProblemSet2.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ProblemSet2 {
 	public static void main (String[] args) {
 		ProblemSet2 ps2 = new ProblemSet2();
@@ -9,10 +11,36 @@ public class ProblemSet2 {
 	}
 	
 	public void sayHello() {
-		
+		Scanner in = new Scanner(System.in);
+		String name = in.nextLine();
+		System.out.println("Hello, " + name);
+		in.close();
 	}
 	
 	public void gradeMe() {
+		Scanner in = new Scanner(System.in);
+		System.out.println("What is your first homework grade?");
+		double homework1 =  in.nextDouble();
+		System.out.println("What is your second homework grade?");
+		double homework2 =  in.nextDouble();
+		System.out.println("What is your third homework grade?");
+		double homework3 =  in.nextDouble();
+		
+		System.out.println("What is your first quiz grade?");
+		double quiz1 =  in.nextDouble();
+		System.out.println("What is your second quiz grade?");
+		double quiz2 =  in.nextDouble();
+		System.out.println("What is your third quiz grade?");
+		double quiz3 =  in.nextDouble();
+		
+		System.out.println("What is your first test grade?");
+		double test1 =  in.nextDouble();
+		System.out.println("What is your second test grade?");
+		double test2 =  in.nextDouble();
+		System.out.println("What is your third test grade?");
+		double test3 =  in.nextDouble();
+		
+		double finalGrade = double (homework1 + homework2 + homework3) / 3; 
 		
 	}
 	
